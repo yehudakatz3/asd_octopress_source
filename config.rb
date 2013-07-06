f9feb2bc8cf4a80323dc68086aaf073cb0b19938
yehudakatz3 <yehuda.katz1@aol.com>
2013-07-06 09:51:40 +0300

    init

---
+++ config.rb
@@ -0,0 +1,18 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/asd/"
+http_images_path = "/asd/images"
+http_fonts_path = "/asd/fonts"
+css_dir = "public/asd/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
